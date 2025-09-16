ecee145456e68fff1ac425f7c513e43c9f5727fb
Benni Valentine <bvalentine@wpi.edu>
2025-09-16 12:28:32 -0400

    Rename directories

--- team03/project1/variant3.py
+++ team03/project1/variant3.py
@@ -1,6 +1,6 @@
 # This is necessary to find the main code
 import sys
-sys.path.insert(0, '../../bomberman')
+sys.path.insert(0, '../../Bomberman')
 sys.path.insert(1, '..')
 
 # Import necessary stuff
@@ -15,10 +15,10 @@ from testcharacter import TestCharacter
 # Create the game
 random.seed(123) # TODO Change this if you want different random choices
 g = Game.fromfile('map.txt')
-g.add_monster(SelfPreservingMonster("aggressive", # name
-                                    "A",          # avatar
-                                    3, 13,        # position
-                                    2             # detection range
+g.add_monster(SelfPreservingMonster("selfpreserving", # name
+                                    "S",              # avatar
+                                    3, 9,             # position
+                                    1                 # detection range
 ))
 
 # TODO Add your character
